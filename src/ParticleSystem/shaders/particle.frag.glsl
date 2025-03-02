a1b89aaad588467763aa300b28a0d8baa257fc2b
Виталий Сосновский <udaaff@gmail.com>
2025-03-02 16:55:34 +0200

    fix: correct color tinting in fragment shader for better color blending

--- src/ParticleSystem/shaders/particle.frag.glsl
+++ src/ParticleSystem/shaders/particle.frag.glsl
@@ -10,8 +10,7 @@ varying vec3 vColor;
 
 void main() {
   vec4 texColor = texture2D(uTexture, vUv);
-  vec3 tint = vColor.r < 0.0 ? vec3(1.0) : (vec3(1.0) + vColor);
-  vec3 color = texColor.rgb * tint;
+  vec3 color = texColor.rgb * vColor; // Корректное применение tint
   #ifdef USE_OPACITY
   gl_FragColor = vec4(color, texColor.a * vOpacity);
   #else
